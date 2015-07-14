7feeefbf0292c2540704c02466cd494e6a15224c
AdNecrias <adnecrias+WhiteRafa@gmail.com>
2015-07-14 21:23:09 +0100

    Reachability Commit 1

--- PCGCG/PCGCG_analizer/src/pcgcg/Cell.java
+++ PCGCG/PCGCG_analizer/src/pcgcg/Cell.java
@@ -6,14 +6,21 @@ public class Cell {
 	public int ballArea = 0;
 	public int cubeArea = 0;
 	public boolean fitsBall = false;
+	public boolean fitsCube = false;
+	public boolean reachBall = false;
+	public boolean reachCube = false;
+	public boolean reachCoop = false;
 	public Point2D topleft, botright;
 	public double sizeX, sizeY;
 	public boolean occupied= false;
 	public int closestOccupied = Integer.MAX_VALUE;
 	public int closestOccupiedX = Integer.MAX_VALUE;
 	public int closestOccupiedY = Integer.MAX_VALUE;
-	public boolean fitsCube = false;
 	public boolean coop = false;
+	public boolean traversedCubeLeft, traversedCubeRight;
+	public int ballVisits = 0;
+	public boolean traversedBallLeft;
+	public boolean traversedBallRight;
 	
 	public Cell( int leftx, int topy, int rightx, int boty ) {
 		topleft = new Point2D.Double(leftx,topy);
